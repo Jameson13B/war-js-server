f82a02cc5fe3e0258c070e38745913f9ada6dfcf
Jameson.Brown <jameson.brown@mx.com>
2021-01-12 15:36:28 -0700

    added README and gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# War.js Server
+
+## Description
+
+A classic card game for all ages, War. This digital version allows two people to play against each other remotely from anywhere with an internet connection and a browser.
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const server = require("express")();
 const cors = require("cors");
 server.use(cors());
 const http = require("http").createServer(server);
+const axios = require("axios");
 const io = require("socket.io")(
   http,
   (options = {
@@ -20,18 +21,42 @@ io.on("connection", function (socket) {
   if (players.length === 1) {
     io.emit("isPlayerA");
   }
+  if (players.length === 2) {
+    io.emit("twoPlayersIn");
+  }
 
+  /**
+ * Deal Cards
+    - 26 cards are dealt for each player as an array of codes
+    Ex. [AH, 3S, 7D, QC]
+ */
   socket.on("dealCards", function () {
-    io.emit("dealCards");
+    if (players.length !== 2) {
+      return io.emit("twoPlayersReq");
+    }
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+      .then(({ data }) => {
+        let cardsA = [];
+        let cardsB = [];
+
+        data.cards.map((card, i) =>
+          i % 2 === 1 ? cardsA.push(card.code) : cardsB.push(card.code)
+        );
+
+        io.emit("dealCards", { cardsA, cardsB });
+      });
   });
 
-  socket.on("cardPlayed", function (gameObject, isPlayerA) {
-    io.emit("cardPlayed", gameObject, isPlayerA);
+  socket.on("cardPlayed", function (gameObject, code, isPlayerA) {
+    io.emit("cardPlayed", gameObject, code, isPlayerA);
   });
 
   socket.on("disconnect", function () {
     console.log("A user disconnected: " + socket.id);
     players = players.filter((player) => player !== socket.id);
+    io.emit("playerAbandoned");
+    io.emit("isPlayerA");
   });
 });
 
